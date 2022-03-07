160f93b69449941d14a1606fbfb4337107d48300
icepick4 <remi.jara@laposte.net>
2022-03-07 11:57:45 +0100

    ajout best score + saut +/- fort en fonction du relachement + pv + gestion du démarrage de partie

--- main.py
+++ main.py
@@ -11,8 +11,13 @@ screen = pygame.display.set_mode(windowSize)
 width = windowSize[0]
 height = windowSize[1]
 
+#timer pour obtenir les ticks
 timer = pygame.time.Clock()
+
+#variable qui maintient le while
 playing = True
+
+
 #personnage
 sonic1Surface = pygame.image.load("images/sonic1.gif").convert_alpha()
 sonic1Rect = entity(sonic1Surface.get_rect(topleft=(100,height - 200 - 144)))
@@ -24,28 +29,51 @@ sonic4Surface = pygame.image.load("images/sonic4.gif").convert_alpha()
 sonic4Rect = entity(sonic4Surface.get_rect(topleft=(100,height - 200 - 144)))
 sonicJumpSurface = pygame.image.load("images/sonicJump.png").convert_alpha()
 sonicJumpRect = entity(sonicJumpSurface.get_rect(topleft=(100,height - 200 - 144*4)))
+#rect qui restreint le personnage
+sonicRect = pygame.Rect((100,0), (128,height - 200))
+#état qui définie quel image du gif on affiche
 sonicState = 0
 #init du delais d'affichage du gif
 timeGif = time()
-#init du départ du saut
 
+
+#init du départ du saut
 startJump = time()
 #temps de saut
 timeJump = 0.3
 #état de saut
 jumping = False
-#rect qui restreint le personnage
-sonicRect = pygame.Rect((100,0), (128,height - 200))
 
-#enemies
+#init des coeurs
+heart3Surface = pygame.image.load("images/damage3.png").convert_alpha()
+heart3Rect = heart3Surface.get_rect(topleft=(65,65))
+heart2Surface = pygame.image.load("images/damage2.png").convert_alpha()
+heart2Rect = heart2Surface.get_rect(topleft=(65,65))
+heart1Surface = pygame.image.load("images/damage1.png").convert_alpha()
+heart1Rect = heart1Surface.get_rect(topleft=(65,65))
+
+#init des enemies
 enemySurface = pygame.image.load("images/poisson-globe.png").convert_alpha()
+enemy2Surface = pygame.image.load("images/league.png").convert_alpha()
 enemies = []
 timeEnemies = time()
-#score
+
+#état de dégat pour effet visuel (fond rouge)
+damage = False
+damageTime = time()
+#état qui définit si on a perdu ou non
+lost = False
+
+#affichage du score du last score et du best score
 scoreTimer = time()
 scoreFont = pygame.font.SysFont("Courier New", 50)
 scoreSurface = scoreFont.render("Score : {0}".format(0), True, (0,0,0))
-scoreRect = scoreSurface.get_rect(midtop=(width/2, 100))
+scoreRect = scoreSurface.get_rect(midtop=(width/2, 125))
+lastScoreSurface = scoreFont.render("Last score : {0}".format(0), True, (0,0,0))
+lastScoreRect = lastScoreSurface.get_rect(midtop=(width/2, 50))
+bestScore = 0
+bestScoreSurface = scoreFont.render("Best score : {0}".format(bestScore), True, (255,25,25))
+bestScoreRect = bestScoreSurface.get_rect(midtop=(width/2, 0))
 #bouton pour fermer la fenetre
 endFont = pygame.font.SysFont("Courier New", 50)
 endSurface = endFont.render("CLOSE", True, (0,0,0))
@@ -61,51 +89,123 @@ while playing:
                 playing = False
         elif event.type == KEYDOWN:
             if event.key == K_SPACE:
+                #on peut sauter
                 if onFloor(sonicJumpRect):
                     startJump = time()
                     jumping = True
-                    jumpSpeed(sonicJumpRect, (0,1200))
+                    jumpSpeed(sonicJumpRect, (0,1450))
+                    jumpSound = pygame.mixer.Sound("sons/jump.mp3")
+                    jumpSound.play()
+                    jumpSound.set_volume(0.03)
+                #on a perdu -> on recommence une partie
+                if lost:
+                    lost = False
+                    scoreTimer = time()
+                    score = 0
+        elif event.type == KEYUP:
+            if event.key == K_SPACE:
+                sonicJumpRect['speed'] = (0,sonicJumpRect['speed'][1] - 500)
+
+    #score et bouton close
+    #remplissage des ennemies
     delayEnemies = time() - timeEnemies
-    if len(enemies) < 2 and delayEnemies >  uniform(0.9,1.5):
+    if len(enemies) < 3 and delayEnemies > uniform(0.6,1.6) and not lost:
         randomSpawn = randint(1,6)
+        randomMob = randint(1,2)
         if randomSpawn == 1:
-            enemies.append(entity(enemySurface.get_rect(topright=(width, height - 200 - 150))))
+            if randomMob == 1:
+                enemies.append(entity(enemySurface.get_rect(topleft=(width, height - 200 - sonic1Surface.get_size()[1]))))
+                enemies[len(enemies)-1]['hp'] = 44
+            else:
+                enemies.append(entity(enemy2Surface.get_rect(topleft=(width, height - 200 - sonic1Surface.get_size()[1]))))
+                enemies[len(enemies)-1]['hp'] = 666
         else:
-            enemies.append(entity(enemySurface.get_rect(topright=(width, height - 200))))
+            if randomMob == 1:
+                enemies.append(entity(enemySurface.get_rect(topleft=(width, height - 200))))
+                enemies[len(enemies)-1]['hp'] = 44
+            else:
+                enemies.append(entity(enemy2Surface.get_rect(topleft=(width, height - 200))))
+                enemies[len(enemies)-1]['hp'] = 666
         timeEnemies = time()
+
+    #tick de la frame
     jumpTime = timer.tick(120) / 1000
-    score = int(round((time() - scoreTimer) * 10,0))
-    scoreSurface = scoreFont.render("Score : {0}".format(score), True, (0,0,0))  
-    screen.fill((255,255,255))
+    
+    #si on a pas perdu on affiche le score actuel, sinon le last score
+    if not lost:
+        score = int(round((time() - scoreTimer) * 10,0))
+        scoreSurface = scoreFont.render("Score : {0}".format(score), True, (0,0,0))
+    else:
+        scoreSurface = scoreFont.render("Score : {0}".format(0), True, (0,0,0))
+
+    #on affiche l'effet visuel (fond rouge) pendant 0.25s
+    damageDelay = time() - damageTime
+    if damage and damageDelay < 0.25:  
+        screen.fill((255,100,100))
+    else:
+        screen.fill((255,255,255))
+        damageTime = time()
+        damage = False
+
+    #affichage du coeur en fonction des pv de sonic
+    if sonic1Rect['hp'] == 3:
+        screen.blit(heart3Surface,heart1Rect)
+    elif sonic1Rect['hp'] == 2:
+        screen.blit(heart2Surface,heart2Rect)
+    else:
+        screen.blit(heart1Surface,heart3Rect)
+
+    for i in range(len(enemies)):
+        if enemies[i-1]['speed'] == (0,0):
+            jumpSpeed(enemies[i-1],(900 + (score * 1.5),0))
+        jumpPosition(enemies[i-1], jumpTime)
+        #si un ennemie touche sonic ...
+        if enemies[i-1]['rect'].colliderect(sonicJumpRect['rect']):
+            damageSound = pygame.mixer.Sound("sons/damage.mp3")
+            damageSound.play()
+            damageSound.set_volume(0.1)
+            damage = True
+            sonic1Rect['hp'] -=1
+            enemies.pop(i-1)
+        elif enemyRestriction(enemies[i-1]):
+            enemies.pop(i-1)
+        if enemies[i-1]['hp'] == 44:
+            screen.blit(enemySurface, enemies[i-1]['rect'])
+        else:
+            screen.blit(enemy2Surface,enemies[i-1]['rect'])
+    
     screen.blit(endSurface,endRect)
     screen.blit(scoreSurface,scoreRect)
-    for i in range(len(enemies)):
-        try:
-            if enemies[i]['speed'] == (0,0):
-                jumpSpeed(enemies[i],(700,0))
-            jumpPosition(enemies[i], jumpTime)
-            print(sonicJumpRect['rect'])
-            if enemies[i]['rect'].colliderect(sonicJumpRect['rect']):
-                sonic1Rect['hp'] -=1
-                enemies.pop(i)
-            elif enemyRestriction(enemies[i]):
-                enemies.pop(i)
-            screen.blit(enemySurface, enemies[i]['rect'])
-        except:
-            pass
-
+    screen.blit(lastScoreSurface,lastScoreRect)
+    screen.blit(bestScoreSurface,bestScoreRect)
+    if bestScore < score :
+        bestScore = score
+        bestScoreSurface = scoreFont.render("Best score : {0}".format(bestScore), True, (255,25,25))
+    #on a perdu
+    if sonic1Rect['hp'] == 0:
+        for i in range(len(enemies)):
+            enemies.pop(0)
+        lost = True
+        lastScore = score
+        lastScoreSurface = scoreFont.render("Last score : {0}".format(lastScore), True, (0,0,0))
+        sonic1Rect['hp'] = 3
+        
+    
     pygame.draw.rect(screen, (0,128,0), (0, height - 200, width, height - 200))
     if time() - startJump < timeJump:
         jumpPosition(sonicJumpRect, jumpTime)
     else:
-        jumpSpeed(sonicJumpRect, (0,-1200))
+        jumpSpeed(sonicJumpRect, (0,-1450))
         startJump = time()
+    #on restreint les positions de sonic
     posRestriction(sonicJumpRect, sonicRect)
-    
+    #si la speed est passé à 0 -> le saut n'est plus actif
     if sonicJumpRect['speed'][1] == 0:
         jumping = False
+    
+    #si on saute on affiche sonicJump, sinon le gif
     if jumping:
-        sonicJumpRect['speed'] = (0,sonicJumpRect['speed'][1] - 12)
+        sonicJumpRect['speed'] = (0,sonicJumpRect['speed'][1] - 15)
         screen.blit(sonicJumpSurface, sonicJumpRect['rect'])
     else:
         #affichage du gif à la main
@@ -140,7 +240,6 @@ while playing:
             else:
                 screen.blit(sonic4Surface,sonic4Rect['rect'])
                 sonicState = 4
-
     pygame.display.flip()
 pygame.quit()
 
